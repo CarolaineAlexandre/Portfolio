52d06f4d0fa43023ac8020199b5c40c815fa2b35
CarolaineAlexandre <carolaine.ferrazemp@gmail.com>
2022-12-19 16:30:49 -0300

    atualizacao II

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const alertList = document.querySelectorAll('.alert')
+const alerts = [...alertList].map(element => new bootstrap.Alert(element))
+
+const bsAlert = new bootstrap.Alert('#myAlert')
+
+const myAlert = document.getElementById('myAlert')
+myAlert.addEventListener('closed.bs.alert', event => {
+
+})
