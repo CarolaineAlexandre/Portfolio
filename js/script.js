4b1b2acb6f0727740a562f64eee700bc41e98871
CarolaineAlexandre <carolaine.ferrazemp@gmail.com>
2022-12-23 19:00:48 -0300

    atualizacao IV

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// ativação do tooltip 
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
